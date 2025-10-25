8c8876f8be8228568551e29e873a40b6f2bac740
DemianMH <za19011250@zapopan.tecmm.edu.mx>
2025-10-24 18:00:49 -0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
   return (
     <html lang="es" className={`${openSans.variable} ${oswald.variable}`}>
       <body className="bg-brand-gray-lighter text-brand-gray-dark font-sans">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="INAPECC" />
+        <link rel="manifest" href="/site.webmanifest" />
         <Header />
         <main>{children}</main>
         <Footer />
